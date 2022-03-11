32435a096b71889a6ff4c58865bf46c6980ed072
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-03-11 11:46:42 -0600

    Created function logic for AddSong.jsx file.

--- src/Componets/AddSong/AddSong.jsx
+++ src/Componets/AddSong/AddSong.jsx
@@ -1,3 +1,28 @@
 import React, { useState } from "react";
 import "./AddSong.css"
 
+const AddSong = (props) => {
+
+    const [title, setTitle]=useState('');
+    const [artist, setArtist]=useState('');
+    const [album, setAlbum]=useState('');
+    const [release_date, setReleaseDate]=useState('');
+    const [genre, setGenre]=useState('');
+
+    function handleSong(event){
+        event.preventDefault();
+        let newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            release_date: release_date,
+            genre: genre,
+        };
+        console.log(newSong);
+    }
+    return ( 
+    <div></div>
+     )
+}
+ 
+export default AddSong;
